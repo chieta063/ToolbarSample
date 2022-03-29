d649833a7d100754b6b6d8647c3855962fd4bf14
阿部紘明 <chieta063@gmail.com>
2022-03-29 18:14:19 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 阿部 紘明 on 2022/03/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/ToolbarSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarSampleApp.swift
+//  Shared
+//
+//  Created by 阿部 紘明 on 2022/03/29.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
